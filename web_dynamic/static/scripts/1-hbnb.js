2c83d8490d4e653631764a30a6621edba001e332
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-25 16:06:51 -0500

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('input').css('margin-right', '10px');
+    const dic = [];
+    $(':checkbox').change(function () {
+        if (this.checked) {
+            dic.push($(this).attr("data-id"));
+        } else {
+            dic.pop($(this).attr("data-id"));
+        }
+    });
+    const name = [];
+    $(':checkbox').change(function () {
+        if (this.checked) {
+            name.push($(this).attr("data-name"));
+        } else {
+            name.pop($(this).attr("data-name"));
+        }
+        if ($.isEmptyObject(name)) {
+            $('.amenities h4').html('&nbsp');
+        } else {
+            $('.amenities h4').text(Object.values(name).join(', '));
+        }
+    });
+});

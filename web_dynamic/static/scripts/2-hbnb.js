5828547e019dc9bec38d6c5ab87060d47771ac7d
Jagucan <jjgc.2499@gmail.com>
2023-04-26 10:57:45 -0500

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+$(function () {
+  $('input').css('margin-right', '10px');
+  const dic = [];
+  $(':checkbox').change(function () {
+    if (this.prop('checked') == true) {
+      dic.push($(this).attr('data-id'));
+    } else {
+      dic.pop($(this).attr('data-id'));
+    }
+  });
+  const name = [];
+  $(':checkbox').change(function () {
+    if (this.prop('checked') == true) {
+      name.push($(this).attr('data-name'));
+    } else {
+      name.pop($(this).attr('data-name'));
+    }
+    if ($.isEmptyObject(name)) {
+      $('.amenities h4').html('&nbsp');
+    } else {
+      $('.amenities h4').text(Object.values(name).join(', '));
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'ok') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+});

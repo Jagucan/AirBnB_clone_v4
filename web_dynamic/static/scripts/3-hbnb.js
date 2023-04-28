14e6e2119595499b5d77e309ae878ed5a1e05d31
Jagucan <jjgc.2499@gmail.com>
2023-04-28 12:41:09 -0500

    task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -21,7 +21,7 @@ $(function () {
       $('.amenities h4').text(Object.values(name).join(', '));
     }
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
@@ -29,7 +29,7 @@ $(function () {
     }
   });
   $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    url: 'http://localhost:5001/api/v1/places_search',
     type: 'POST',
     contentType: 'application/json',
     data: JSON.stringify({}),
@@ -49,4 +49,25 @@ $(function () {
       });
     },
   });
+  /* 
+  $.ajax({
+        url: 'http://localhost:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: '{}',
+        dataType: 'json',
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                let place = data[i];
+                $('.places').append('<article><h2>' + place.name + '</h2>\
+                    <div class="price_by_night"><p>$' + place.price_by_night + '</p>\
+                    </div><div class="information"><div class="max_guest"><div class="guest_image"></div>\
+                    <p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div>\
+                    <p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div>\
+                    <p>' + place.number_bathrooms + '</p></div></div>\
+                    <div class="description"><p>' + place.description + '</p></div></article>');
+            };
+        }
+    });
+    */
 });
